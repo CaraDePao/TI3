03a0a40176a5ad0f5b68096791ca6ccd35431bbf
CaraDePao <pedromartins2013@gmail.com>
2025-02-03 17:43:59 -0300

    Initial commit

---
+++ Assets/ReadMe.cs
@@ -0,0 +1,3 @@
+//Whenever you create any object, material, folder, etc. Their naming convention must not have spaces between letters, special characters such as "_" or numbers
+//and must include a capitalized letter at the beggining of every word that's supposed to be separated by spaces.
+//example: MyFolderNumberOne  (correct), My Folder Number_1 (incorrect), Myfoldernumberone (incorrect)
